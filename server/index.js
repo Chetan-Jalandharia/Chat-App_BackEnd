113a86eb81e3def2904f758da1d1e92f7304cc94
chetan <chetann8900@gmail.com>
2023-06-29 15:14:27 +0530

    cors test 4

--- server/index.js
+++ server/index.js
@@ -9,27 +9,30 @@ const app = express()
 
 // database initializaion
 const db = require('./Config/db')
-const corsOpts = {
-    origin: '*',
+// const corsOpts = {
+//     origin: '*',
 
-    methods: [
-        'GET',
-        'POST',
-    ],
+//     methods: [
+//         'GET',
+//         'POST',
+//     ],
 
-    allowedHeaders: [
-        'Content-Type',
-    ],
-};
+//     allowedHeaders: [
+//         'Content-Type',
+//     ],
+// };
 
 //middelware initialization
-app.use(cors(corsOpts))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
     next();
 });
 
